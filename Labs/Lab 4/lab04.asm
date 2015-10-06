7335c1a2139ae8514f124723474f02cfac38b5fe
Paul Davis <pauldavis@pitt.edu>
2015-10-05 21:07:39 -0400

    Finished lab4

--- Labs/Lab 4/lab04.asm
+++ Labs/Lab 4/lab04.asm
@@ -16,10 +16,12 @@
 		la $a0, hello
 		syscall
 	Main:
+		#$a0 = address of an input buffer
+		#$a1 = maximum number of characters to read into the buffer
 		la $a0, buffer
 		li $a1, 64
-		jal _readString
-		addi $s0, $v0, 0 #Length of the string
+		jal _readString		#Jump to readstring
+		addi $s0, $v0, 0	#Length of the string
 		
 		addi $v0, $zero, 4
 		la $a0, str1
@@ -39,7 +41,7 @@
 		
 		addi $v0, $zero, 5
 		syscall
-		add $s1, $zero, $v0 #Lower
+		add $s1, $zero, $v0 #Get value for start bounds
 		
 		addi $v0, $zero, 4
 		la $a0, end
@@ -47,7 +49,7 @@
 		
 		addi $v0, $zero, 5
 		syscall
-		add $s2, $zero, $v0 #Higher
+		add $s2, $zero, $v0 #Get value for end bounds
 		
 		addi $v0,$zero,4
 		la $a0, sub
@@ -73,11 +75,6 @@
 		syscall
 
 
-
-		
-
-
-
 	_readString:
 		addi $t9, $ra, 0	# Save location jumped from
 		addi $v0, $zero, 8	# Readin string
@@ -88,9 +85,9 @@
 		lb $t1, buffer($t0)	# Set t1 equal to the t0 char of string
 		addi $t0, $t0, 1	# counter++
 		bne $t1, $zero, removeLoop # Restart loop until null is reached
-		beq $a1, $s0, skip	# 
-		subi $t0, $t0, 2	# 
-		sb $zero, buffer($t0)	# 
+		beq $a1, $s0, skip
+		subi $t0, $t0, 2
+		sb $zero, buffer($t0)
 	skip:
 		jal _strLength
 		addi $ra, $t9, 0
@@ -98,52 +95,56 @@
 
 
 	_strLength:
-		li $t0, 0 		# initialize the count to zero
+		li $t0, 0 			# initialize the count to zero
 	loop:
 		lb $t1, 0($a0) 		# load next character into t1
 		beq $t1, 0, exit	# check for null character
 		addi $a0, $a0, 1 	# increment string pointer
 		addi $t0, $t0, 1 	# count++
-		j loop 			# loop
+		j loop 				# loop
 	exit:
 		addi $v0, $t0, 0	
-		jr $ra			# return
+		jr $ra				# return
 
 
+
+		#a0 = address of an input string
+		#$a1 = address of an output buffer
+		#$a2 = start index for the input string (inclusive)
+		#$a3 = end index for the input string (exclusive)
 	_subString:
-		addi $t9, $ra,0
+		addi $t9, $ra, 0
 		addi $t8, $a1, 0
 		addi $t7, $zero, 0	#Counter for substring
 		addi $t6, $a2, 0	#Counter for string
-		slti $t0, $a2, 0
+		slti $t0, $a2, 0	
 		bne $t0, $zero, null
 		slti $t0, $a3, 0
 		bne $t0, $zero, null
 		slt $t0, $a2, $s0
 		beq $t0, $zero, null
 		slt $t0, $a3, $s0
-		beq $t0, $zero, higherBound
-	returnHigherBound:
+		beq $t0, $zero, higherBound #If counter is 0, goto higher bound
+	returnHB:
 		slt $t0, $a2, $a3
 		beq $t0, $zero, null
 		j subloop
 		
 	higherBound:
 		addi $a3, $s0, 0
-		j returnHigherBound
-			
+		j returnHB	
 	subloop:
-		lb $t1,buffer($t6)
+		lb $t1, buffer($t6)
 		sb $t1, substring($t7)
-		addi $t6,$t6,1
-		addi $t7,$t7,1
-		beq $t6,$a3,null
+		addi $t6, $t6, 1
+		addi $t7, $t7, 1
+		beq $t6, $a3, null
 		j subloop
 	null:
 		sb $zero substring($t7)
-		j finish
+		j ending
 								
-	finish:
+	ending:
 		addi $ra, $t9, 0
 		addi $v0, $t8, 0
 		jr $ra
