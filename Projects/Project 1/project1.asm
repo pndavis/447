87fd477d4b9e244d353e3cb2df74291117a6d355
Paul Davis <pauldavis@pitt.edu>
2015-10-14 16:39:04 -0400

    More bug fixes on project 1, added files for project 2

--- Projects/Project 1/project1.asm
+++ Projects/Project 1/project1.asm
@@ -1,16 +1,11 @@
 .text
 
-# You can only use registers $s0 to $s7 and $t0 to $t9 in your program.
-
 # $t0 - Opp 1
 # $t1 - Opp 2
 # $t2 - Opperator
 # $t3 - Result
-
 # $t8 - display
 # $t9 - key pad
-
-
 
 state0: #reset
 	addi $t9, $zero, 0 #Set Keypad to 0
@@ -20,102 +15,75 @@ state0: #reset
 	addi $t2, $zero, 0 #Opperator
 	addi $t3, $zero, 0 #Result
 	add $t8, $zero, $t1
-
 wait:
 	beq $t9, $zero, wait # Waits for a button to be pressed until continuing 
-	
 state1:
 	sll $t9, $t9, 1 
-	srl $t9, $t9, 1 # These two lines get rid of the 1 at the beginning of t9
-	
-	
+	srl $t9, $t9, 1 # These two lines get rid of the 1 at the beginning of t9	
 	slti $s1, $t9, 10 #if the values are 0-9 we will do different things that about 9
 	beq $s1, $zero, opp1
-	
 num1:
 	sll $t4, $t0, 1
 	sll $t5, $t0, 3
 	add $t0, $t4, $t5	# operand1 = (operand1 * 10) + Input
-	add $t0, $t0, $t9
-		
+	add $t0, $t0, $t9	
 	add $t8, $zero, $t0 	# Display operand1
-	add $t9, $zero, $zero
-	
+	add $t9, $zero, $zero	
 	j wait			# Go back to State 1
-	
 opp1:	
 	beq $t9, 15, state0	# C restarts program
 	beq $t9, 14, equals	#
-	
 	add $t2, $zero, $t9	# operator = Input
 	add $t8, $zero, $t0	# Display operand1
 	add $t9, $zero, $zero	# reset t9
 	j wait2			# Go to State 2
-	
 equals:
 	add $t3, $zero, $t0	# result = operand1
 	add $t8, $zero, $t3	# Display result
-	j state4		# Go to State 4
-
-	
-		
+	j state4		# Go to State 4	
 			
 wait2:
 	beq $t9, $zero, wait2
-	
 state2:
 	sll $t9, $t9, 1 
 	srl $t9, $t9, 1 	# These two lines get rid of the 1 at the beginning of t9
-	
 	slti $s1, $t9, 10 	#if the values are 0-9 we will do different things that about 9
 	beq $s1, $zero, opp2
-
 num2:
 	sll $t4, $t1, 1
 	sll $t5, $t1, 3
 	add $t1, $t4, $t5	# operand2 = (operand2 * 10) + Input
-	add $t1, $t1, $t9
-		
+	add $t1, $t1, $t9	
 	add $t8, $zero, $t1	# Display operand2
-	add $t9, $zero, $zero	# Clear t9
-	
+	add $t9, $zero, $zero	# Clear t9	
 	j wait3
-
 opp2:	
 	beq $t9, 15, state0	# C restarts program
 	beq $t9, 14, equals2
-	
 	add $t2, $zero, $t9	# operator = Input
 	add $t8, $zero, $t0	# Display operand1
 	add $t9, $zero, $zero	# reset t9
 	j wait2			# Go to State 2
-	
 equals2:
 	add $t3, $zero, $t0	# result = operand1
 	add $t8, $zero, $t3	# Display result
 	j state4		# Go to State 4
 
-
 wait3:
 	beq $t9, $zero, wait3
 state3:
 	sll $t9, $t9, 1 
-	srl $t9, $t9, 1 # These two lines get rid of the 1 at the beginning of t9
-	
-	slti $s1, $t9, 10 #if the values are 0-9 we will do different things that about 9
+	srl $t9, $t9, 1 	# These two lines get rid of the 1 at the beginning of t9
+	slti $s1, $t9, 10 	#if the values are 0-9 we will do different things that about 9
 	beq $s1, $zero, opp3
-	
 num3:
 	sll $t4, $t1, 1
 	sll $t5, $t1, 3
 	add $t1, $t4, $t5	# operand2 = (operand2 * 10) + Input
 	add $t1, $t1, $t9
-
 	add $t8, $zero, $t1	# Display operand2
-	add $t9, $zero, $zero	# Clear t9
-	
+	add $t9, $zero, $zero	# Clear t9	
 	j wait3
-
 opp3:	
 	beq $t9, 15, state0	# C restarts program
 	addi $s5, $zero, 0	# Multiplication Counter
@@ -124,7 +92,6 @@ opp3:
 	beq $t2, 11, subtraction
 	beq $t2, 12, multiplication
 	beq $t2, 13, division
-	
 addition:
 	add $t3, $t0, $t1
 	j finishCalc
@@ -158,31 +125,29 @@ divisionNeg:
 	j divisionNeg	
 
 finishCalc:
+	addi $s5, $zero, 0	# Multiplication Counter
+	addi $s6, $zero, 0
 	add $t8, $t3, $zero 	# Displays results
 	beq $t9, 14, equals3
 	addi $t0, $t3, 0
 	add $t3, $zero, $zero	# Reset results
 	add $t1, $zero, $zero	
-	
 	add $t2, $t9, $zero	# Operator = Input
 	j state2
-	
 equals3:
+	add $t8, $t3, $zero
 	add $t2, $zero, $zero
 	add $t1, $zero, $zero
 	add $t9, $zero, $zero	
 	j wait4
-	
 
 wait4:
 	beq $t9, $zero, wait4
 state4:
 	sll $t9, $t9, 1 
 	srl $t9, $t9, 1 # These two lines get rid of the 1 at the beginning of t9
-	
 	slti $s1, $t9, 10 #if the values are 0-9 we will do different things that about 9
 	beq $s1, $zero, opp4
-
 num4:
 	add $t8, $zero, $zero
 	add $t0, $t9, $t0	#operand1 = Input
@@ -193,8 +158,8 @@ opp4:
 	beq $t9, 15, state0	# C restarts program
 	beq $t9, 14, equals4
 	
-	add $t0, $t3, $t9	# operator = Input
-	add $t8, $zero, $t0	# Display operand1
+	add $t0, $zero, $t3	#operand1 = result
+	add $t2, $zero, $t9	#operator = Input
 	add $t9, $zero, $zero	# reset t9
 	j wait2			# Go to State 2
 equals4:
