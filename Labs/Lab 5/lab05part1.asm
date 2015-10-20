5751961733b86cf5e3435dc51f5acbc81c00e194
Paul Davis <pauldavis@pitt.edu>
2015-10-20 18:55:02 -0400

    Working on lab 5

--- Labs/Lab 5/lab05part1.asm
+++ Labs/Lab 5/lab05part1.asm
@@ -1,1 +1,27 @@
-a
+
+.data
+	names: .asciiz "steve", "john", "chelsea", "julia", "ryan" 
+	ages: .byte 20, 25, 22, 21, 23
+	prompt: .asciiz "Please enter a name: "
+	input: .space 64
+	notFound: .asciiz "Not found!\n"
+	age: .asciiz "Age is: "
+
+.text
+	addi $v0, $zero, 4
+	la $a0, prompt
+	syscall					#Asks users to enter a name
+
+	la $a0, input
+	addi $a1, $zero, 64
+	#jal _readString
+
+	la $s0, input			
+	add $s1, $zero, $zero		
+	la $s2, names	
+
+
+
+
+
+_readString:
--- Labs/Lab 5/lab05part2.asm
+++ Labs/Lab 5/lab05part2.asm
@@ -1,1 +1,61 @@
-b
+.data 
+	prompt: .asciiz "Enter a nonnegative integer: "
+	middle: .asciiz "! = "
+	invalid: .asciiz "Invalid integer; try again.\n"
+
+.text
+
+start:
+	addi $v0, $zero, 4
+	la $a0, prompt
+	syscall					#Print inital prompt
+
+	addi $v0,$zero,5
+	syscall
+
+	add $s0, $zero, $v0
+	slti $t0,$s0,0
+	bne $t0, $zero, failed	#Tests the numbers validity
+
+	add $a0, $zero, $s0
+	jal _Factorial			#Jump to factorial funtion
+
+	add $s1, $zero, $v0
+	div $s1, $s1, $s0
+	addi $v0, $zero, 1
+	add $a0, $zero, $s0
+	syscall
+
+	addi $v0, $zero, 4
+	la $a0, middle
+	syscall
+
+	addi $v0, $zero, 1
+	add $a0, $zero, $s1
+	syscall
+	
+	j end
+
+_Factorial:
+	addi $sp, $sp, -8
+	sw $ra, 4($sp) 
+	sw $a0, 0($sp)
+	ble $a0, $zero, return
+	addi $a0, $a0, -1
+	jal _Factorial
+	lw $a0, 0($sp)
+	mul $v0, $v0, $a0
+return:
+	lw $ra 4($sp)
+    addi $sp, $sp, 8
+	jr $ra
+
+failed:
+	addi $v0, $zero, 4
+	la $a0, invalid
+	syscall					#Says the number is invalid and restarts the program
+	j start
+
+end:
+	addi $v0, $zero, 10
+	syscall
